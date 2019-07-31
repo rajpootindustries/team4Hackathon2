d2bbefaecb879fa770fd10b3a5b6d4b4fa80e82c
Vannarith Sar <vannsar@gmail.com>
2019-07-31 16:06:02 -0700

    updates

--- components/exchange.js
+++ components/exchange.js
@@ -3,21 +3,9 @@ class Exchange{
     this.render = this.render.bind(this);
     this.data = {
       exchangeName : null,
-      //This is the current BUY price listed by the exchange
-      buyBTC: null,
-      //This is the current SELL price listed by the exchange
-      sellBTC: null,
-      //This is the LAST traded price listed by the exchange
       spotBTC: null,
-
-      buyETH: null,
-      sellETH: null,
       spotETH: null,
-
-      buyLTC: null,
-      sellLTC: null,
       spotLTC: null,
-
       makerFees: null,
       takerFees: null,
       exchangeFees: null,
@@ -41,30 +29,23 @@ class Exchange{
     var tableHead = $('<thead>');
     var tableHeadRow = $('<tr>');
     tableHeadRow.append($('<th>').text(this.exchangeName));
-    tableHeadRow.append($('<th>').text('Buy'));
-    tableHeadRow.append($('<th>').text('Sell'));
-    tableHeadRow.append($('<th>').text('Spot'));
+    tableHeadRow.append($('<th>').text('Current Price'));
     tableHead.append(tableHeadRow);
 
     var tableBody = $('<tbody>');
+
     var tableBodyRow = $('<tr>');
     tableBodyRow.append($('<td>').text("Bitcoin"))
-    tableBodyRow.append($('<td>').text('$'+this.data.buyBTC));
-    tableBodyRow.append($('<td>').text('$'+this.data.sellBTC));
     tableBodyRow.append($('<td>').text('$'+this.data.spotBTC));
     tableBody.append(tableBodyRow);
 
     var tableBodyRow = $('<tr>');
     tableBodyRow.append($('<td>').text("Ethereum"))
-    tableBodyRow.append($('<td>').text('$' + this.data.buyETH));
-    tableBodyRow.append($('<td>').text('$' + this.data.sellETH));
     tableBodyRow.append($('<td>').text('$' + this.data.spotETH));
     tableBody.append(tableBodyRow);
 
     var tableBodyRow = $('<tr>');
     tableBodyRow.append($('<td>').text("Litecoin"))
-    tableBodyRow.append($('<td>').text('$' + this.data.buyLTC));
-    tableBodyRow.append($('<td>').text('$' + this.data.sellLTC));
     tableBodyRow.append($('<td>').text('$' + this.data.spotLTC));
     tableBody.append(tableBodyRow);
 
