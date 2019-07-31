310d5dfd07b81b94e136c50847a88cdfbe03bc04
sanjay <32687097+rajpootindustries@users.noreply.github.com>
2019-07-31 14:33:50 -0700

    okEx

--- components/okexchange.js
+++ components/okexchange.js
@@ -13,19 +13,40 @@
 //   $.ajax(ajaxConfig);
 // }
 
-function checkPrice() {
-  var ajaxConfig = {
-    dataType: 'json',
-    method: 'POST',
-    url: 'https://www.bitstamp.net/api/v2/ticker/btcusd/',
-    // data: { "api_key": 'QxABQ0kzHe', name: studentName, course: studentCourse, grade: studentGrade },
-    success: function (response) { console.log('success adding!', response) },
-    error: function () {
-      console.log('an error has occured in adding');
+class Bitstamp extends Exchange {
+  constructor(){
+    super()
+  }
+  checkPrice() {
+    var ajaxConfig = {
+      dataType: 'json',
+      method: 'POST',
+      url: 'https://www.bitstamp.net/api/v2/ticker/btcusd/',
+      // data: { "api_key": 'QxABQ0kzHe', name: studentName, course: studentCourse, grade: studentGrade },
+      success: function (response) { console.log('success adding!', response.last) },
+      error: function () {
+        console.log('an error has occured in adding');
+      }
     }
+
+    $.ajax(ajaxConfig);
   }
 
-  $.ajax(ajaxConfig);
 }
 
-$(document).ready(checkPrice);
+// function checkPrice() {
+//   var ajaxConfig = {
+//     dataType: 'json',
+//     method: 'POST',
+//     url: 'https://www.bitstamp.net/api/v2/ticker/btcusd/',
+//     // data: { "api_key": 'QxABQ0kzHe', name: studentName, course: studentCourse, grade: studentGrade },
+//     success: function (response) { console.log('success adding!', response) },
+//     error: function () {
+//       console.log('an error has occured in adding');
+//     }
+//   }
+
+//   $.ajax(ajaxConfig);
+// }
+
+// $(document).ready(checkPrice);
