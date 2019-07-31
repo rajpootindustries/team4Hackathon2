323d23ec726dc03ce7bcd78ef3d4dc9252a59d6a
Vannarith Sar <vannsar@gmail.com>
2019-07-31 12:09:17 -0700

    added LTC and ETH placeholders in exchange class

--- components/exchange.js
+++ components/exchange.js
@@ -7,6 +7,15 @@ class Exchange {
       sellBTC: null,
       //This is the LAST traded price listed by the exchange
       spotBTC: null,
+
+      buyETH: null,
+      sellETH: null,
+      spotETH: null,
+
+      buyLTC: null,
+      sellLTC: null,
+      spotLTC: null,
+
       makerFees: null,
       takerFees: null,
       exchangeFees: null
