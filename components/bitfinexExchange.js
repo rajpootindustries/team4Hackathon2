9ec3fab413339a9a095dc90015b9acf004272434
Brandon Lee <ledbrandon@yahoo.com>
2019-07-31 17:44:19 -0700

    bitfinex

--- components/bitfinexExchange.js
+++ components/bitfinexExchange.js
@@ -1,9 +1,14 @@
-class Bitfinex {
+class Bitfinex extends Exchange{
   constructor() {
-    this.bitfinexData = {};
-    this.checkPrice();
+    super();
+    this.bitfinexBTC = {};
+    this.bitfinexLTC = {};
+    this.bitfinexETH = {};
+    this.checkPriceBTC();
+    this.checkPriceLTC();
+    this.checkPriceETH();
   }
-  checkPrice() {
+  checkPriceBTC() {
     var ajaxConfig = {
       dataType: 'json',
       method: 'GET',
@@ -13,9 +18,55 @@ class Bitfinex {
       },
       success: function (response) {
         for (var key in response) {
-          this.bitfinexData[key] = response[key];
+          this.bitfinexBTC[key] = response[key];
         }
-        console.log(this.bitfinexData);
+        console.log(this.bitfinexBTC);
+        this.data.spotBTC = this.bitfinexBTC.last_price;
+
+      }.bind(this),
+      error: function () {
+        console.log('an error has occured in adding');
+      }
+    }
+    $.ajax(ajaxConfig);
+  }
+  checkPriceLTC() {
+    var ajaxConfig = {
+      dataType: 'json',
+      method: 'GET',
+      url: '../server/api-proxy-bitfinex.php',
+      data: {
+        id: 'LTCUSD'
+      },
+      success: function (response) {
+        for (var key in response) {
+          this.bitfinexLTC[key] = response[key];
+        }
+        console.log(this.bitfinexLTC);
+        this.data.spotLTC = this.bitfinexLTC.last_price;
+
+      }.bind(this),
+      error: function () {
+        console.log('an error has occured in adding');
+      }
+    }
+  $.ajax(ajaxConfig);
+  }
+  checkPriceETH() {
+    var ajaxConfig = {
+      dataType: 'json',
+      method: 'GET',
+      url: '../server/api-proxy-bitfinex.php',
+      data: {
+        id: 'ETHUSD'
+      },
+      success: function (response) {
+        for (var key in response) {
+          this.bitfinexETH[key] = response[key];
+        }
+        console.log(this.bitfinexETH);
+        this.data.spotETH = this.bitfinexETH.last_price;
+
       }.bind(this),
       error: function () {
         console.log('an error has occured in adding');
