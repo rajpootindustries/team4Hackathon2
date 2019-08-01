51ee879a812be49aff8da0af937e10bdb4b007c0
sanjay <32687097+rajpootindustries@users.noreply.github.com>
2019-08-01 16:02:22 -0700

    getBTC and getETH

--- components/main.js
+++ components/main.js
@@ -10,6 +10,8 @@ function initializeApp(){
       exchangeArray[i].render();
     }
   }, 2000)
+  setTimeout(getBTC, 3000);
+  setTimeout(getETH, 3000);
 }
 
 function initializeExchanges(){
@@ -17,4 +19,42 @@ function initializeExchanges(){
    exchangeArray.push(new exchanges[i]);
  }
  console.log(exchangeArray);
+}
+
+function getBTC (){
+  var returnBTC = {}
+  var max = 0;
+  var min = 10000000;
+  for (var exchanges in exchangeArray){
+    if (parseInt(exchangeArray[exchanges].data.spotBTC) > max){
+      max = parseInt(exchangeArray[exchanges].data.spotBTC);
+      returnBTC.max = parseInt(exchangeArray[exchanges].data.spotBTC);
+      returnBTC.exchangeMaxName = exchangeArray[exchanges].data.exchangeName;
+    }
+    if (parseInt(exchangeArray[exchanges].data.spotBTC) < min ){
+      min = parseInt(exchangeArray[exchanges].data.spotBTC)
+      returnBTC.exchangeMin = parseInt(exchangeArray[exchanges].data.spotBTC);
+      returnBTC.exchangeMinName =exchangeArray[exchanges].data.exchangeName;
+    }
+  }
+  console.log(returnBTC);
+}
+
+function getETH() {
+  var returnETH = {}
+  var max = 0;
+  var min = 10000000;
+  for (var exchanges in exchangeArray) {
+    if (parseInt(exchangeArray[exchanges].data.spotETH) > max) {
+      max = parseInt(exchangeArray[exchanges].data.spotETH);
+      returnETH.max = parseInt(exchangeArray[exchanges].data.spotETH);
+      returnETH.exchangeMaxName = exchangeArray[exchanges].data.exchangeName;
+    }
+    if (parseInt(exchangeArray[exchanges].data.spotETH) < min) {
+      min = parseInt(exchangeArray[exchanges].data.spotETH)
+      returnETH.exchangeMin = parseInt(exchangeArray[exchanges].data.spotETH);
+      returnETH.exchangeMinName = exchangeArray[exchanges].data.exchangeName;
+    }
+  }
+  console.log(returnETH);
 }
